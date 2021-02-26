1af7a8de79a9280a012ab0c0490bcb838bd30f70
Wojtek <clearangel17@gmail.com>
2021-02-26 21:03:38 +0100

    add ichecbox to login as client or as staff member

--- Online_Shop/register_and_login/forms.py
+++ Online_Shop/register_and_login/forms.py
@@ -3,6 +3,7 @@ from django import forms
 class loginForm(forms.Form):
     login = forms.CharField(label = 'login', max_length=100)
     password = forms.CharField(label = 'haslo', max_length=100, widget=forms.PasswordInput())
+    is_staff_member = forms.BooleanField(label="Zaloguj jako pracownik", required=False)
 
 class registerForm(forms.Form):
     name = forms.CharField(label = 'imie', max_length=100)
