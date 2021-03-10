a0057f64ffaf32a0c7fabca4445c2974b252762c
Equilibrium23 <wojciech.jedraski1999@gmail.com>
2021-03-11 00:53:12 +0100

    update

--- Online_Shop/settings.py
+++ Online_Shop/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'h1(2uq*zl$-0$(gy7nd#*kdcw^+@3_juqgncxp$l53ikamx2z#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
